7f0e7ffce8c0d095e15c9220c3587543e355d408
Vanchi Koduvayur <vanchi@zendesk.com>
2013-04-04 18:52:11 -0700

    work request wip

--- ug_guts/req_extract.h
+++ ug_guts/req_extract.h
@@ -3,11 +3,17 @@
 
 #include <time.h>
 
+//States for request
 #define WAITING_PATTERN 0
 #define IN_REQ 1
 #define REQ_COMPLETE 2
-#define EOF_REACHED 3
-#define STOP_SIGNAL 4
+#define EOF_REACHED -1
+#define STOP_SIGNAL -2
+
+// Matcher returns
+#define MATCH_FOUND 1
+#define NO_MATCH 0
+#define SKIP_LINE -1
 
 typedef struct {
     char **buf;
--- ug_guts/ug_guts.c
+++ ug_guts/ug_guts.c
@@ -43,8 +43,39 @@ int check_request(int lines, char **request, time_t request_time, pcre **regexps
 	return(matched);
 }
 
+void print_request(int request_lines, char **request)
+{
+	int i, j;
+	putchar('\n');
 
-int rails_req_match(char *line, ssize_t line_size, time_t* tv) {
+	for(i=0; i < request_lines; i++)
+		printf("%s", request[i]);
+
+	for(j=0; j < strlen(request[request_lines - 1]) && j < 80; j++ )
+		putchar('-');
+
+	putchar('\n');
+	fflush(stdout);
+}
+
+int handle_request(request_t* req, context_t* cxt) {
+    static int tick = 0;
+    if(req->time > cxt->start_time &&
+            check_request(req->lines,  req->buf, req->time, cxt->regexps, cxt->num_regexps)) {
+        printf("@@%lu\n", req->time);
+
+        print_request(req->lines, req->buf);
+    }
+
+    if ( tick % 100 == 0 )
+        printf("@@%lu\n", req->time);
+    tick++;
+    if(req->time > cxt->end_time)
+        return -1;
+    return 0;
+}
+
+int rails_req_matcher(char *line, ssize_t line_size, time_t* tv) {
     const char* error;
     int erroffset;
     int ovector[30];
@@ -64,42 +95,10 @@ int rails_req_match(char *line, ssize_t line_size, time_t* tv) {
         *tv = mktime(&request_tm);
         free(date_buf);
     }
-    return(matched);
+    return((matched>0)?MATCH_FOUND:NO_MATCH);
 }
 
-void print_rails_request(int request_lines, char **request)
-{
-	int i, j;
-	putchar('\n');
-
-	for(i=0; i < request_lines; i++)
-		printf("%s", request[i]);
-
-	for(j=0; j < strlen(request[request_lines - 1]) && j < 80; j++ )
-		putchar('-');
-
-	putchar('\n');
-	fflush(stdout);
-}
-
-int handle_rails_request(request_t* req, context_t* cxt) {
-    static int tick = 0;
-    if(req->time > cxt->start_time &&
-            check_request(req->lines,  req->buf, req->time, cxt->regexps, cxt->num_regexps)) {
-					printf("@@%lu\n", req->time);
-
-                                        print_rails_request(req->lines, req->buf);
-    }
-
-    if ( tick % 100 == 0 )
-        printf("@@%lu\n", req->time);
-    tick++;
-    if(req->time > cxt->end_time)
-        return -1;
-    return 0;
-}
-
-int work_req_match(char* line, ssize_t line_size, time_t* tv) {
+int work_req_matcher(char* line, ssize_t line_size, time_t* tv) {
     const char* error;
     int erroffset;
     int ovector[30];
@@ -107,11 +106,18 @@ int work_req_match(char* line, ssize_t line_size, time_t* tv) {
     struct tm request_tm;
     int matched;
     static pcre* regex= NULL;
+    static pcre* selector_regex= NULL;
     static pcre* time_regex = NULL;
 
     if(regex == NULL) {
+        selector_regex = pcre_compile("resque", 0, &error, &erroffset, NULL);
         regex = pcre_compile("Starting this session", 0, &error, &erroffset, NULL);
         time_regex = pcre_compile("\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}", 0, &error, &erroffset, NULL);
+    }
+
+    //Skip lines without 'resque'
+    if(pcre_exec(selector_regex, NULL, line, line_size, 0, 0, ovector, 30) <= 0) {
+        return(SKIP_LINE);
     }
 
     matched = pcre_exec(regex, NULL, line, line_size,0,0,ovector, 30);
@@ -124,39 +130,7 @@ int work_req_match(char* line, ssize_t line_size, time_t* tv) {
             free(date_buf);
         }
     }
-    return(matched);
-}
-
-void print_work_request(int request_lines, char **request) {
-    int i, j;
-    putchar('\n');
-
-    for(i=0; i < request_lines; i++)
-        printf("%d: %s", i, request[i]);
-
-    for(j=0; j < strlen(request[request_lines - 1]) && j < 80; j++ )
-        putchar('-');
-
-    putchar('\n');
-    fflush(stdout);
-}
-
-int handle_work_request(request_t* req, context_t* cxt) {
-    static int tick = 0;
-    if(req->time > cxt->start_time &&
-            check_request(req->lines,  req->buf, req->time, cxt->regexps, cxt->num_regexps)) {
-        printf("@@%lu\n", req->time);
-
-        print_work_request(req->lines, req->buf);
-    }
-
-    if ( tick % 100 == 0 )
-        printf("@@%lu\n", req->time);
-    tick++;
-    if(req->time > cxt->end_time)
-        return -1;
-    return 0;
-
+    return((matched>0)?MATCH_FOUND:NO_MATCH);
 }
 
 int main(int argc, char **argv)
@@ -168,20 +142,25 @@ int main(int argc, char **argv)
     int erroffset;
     char *line = NULL;
     ssize_t line_size, allocated;
+    char matcher = 'a';
 
-    if ( argc < 4 ) {
-        fprintf(stderr, "Usage: ug_guts start_time end_time regexps [... regexps]\n");
+
+    if ( argc < 5 ) {
+        fprintf(stderr, "Usage: ug_guts (work|app) start_time end_time regexps [... regexps]\n");
         exit(1);
     }
 
     cxt = malloc(sizeof(context_t));
-    cxt->start_time = atol(argv[1]);
-    cxt->end_time = atol(argv[2]);
+    cxt->start_time = atol(argv[2]);
+    cxt->end_time = atol(argv[3]);
+    if(strcmp("work", argv[1]) == 0) {
+        matcher = 'w';
+    }
 
-    cxt->num_regexps = argc - 3;
+    cxt->num_regexps = argc - 4;
     cxt->regexps = malloc(sizeof(pcre *) * cxt->num_regexps);
 
-    for ( i = 3; i < argc; i++) {
+    for ( i = 4; i < argc; i++) {
         cxt->regexps[i-3] = pcre_compile(argv[i], 0, &error, &erroffset, NULL);
         if ( error ) {
             fprintf(stderr, "Error compiling regexp \"%s\": %s\n", argv[i], error);
@@ -191,12 +170,16 @@ int main(int argc, char **argv)
 
     req = malloc(sizeof(request_t));
 
-    req_extractor_init(req, &rails_req_match);
+    if(matcher == 'w') {
+        req_extractor_init(req, &work_req_matcher);
+    }else {
+        req_extractor_init(req, &rails_req_matcher);
+    }
 
     while(1) {
         int ret;
         line_size = getline(&line, &allocated, stdin);
-        ret = req_extract_each_line(line, line_size, req, &handle_rails_request, cxt);
+        ret = req_extract_each_line(line, line_size, req, &handle_request, cxt);
         if(ret == EOF_REACHED || ret == STOP_SIGNAL) {
             break;
         }
