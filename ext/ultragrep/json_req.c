9014eca0aa43d2b111500d21f71d9f540c7412da
Akshara Chaturvedi <achaturvedi@zendesk.com>
2014-01-31 10:17:31 -0800

    Added searching for regular expression in JSON  and  modified pretty print in JSON indented format.

--- ext/ultragrep/json_req.c
+++ ext/ultragrep/json_req.c
@@ -8,12 +8,11 @@
 
 /*
 todo:
-Fix search
-make it work for key filtering
+  Make it work for key filtering
 */
 
 static int lineCount=0;
-static int indentValue = 4;
+static int indentValue = 4; //JSON print uses this
 
 typedef struct {
     req_matcher_t base;
@@ -25,7 +24,6 @@ typedef struct {
 
 } json_req_matcher_t;
 
-
 typedef struct {
     time_t start_time;
     time_t end_time;
@@ -36,6 +34,7 @@ typedef struct {
 
 static request_t request;
 
+//
 static void json_on_request(json_req_matcher_t * m, request_t * r)
 {
     if (r && m->on_request) {
@@ -46,12 +45,14 @@ static void json_on_request(json_req_matcher_t * m, request_t * r)
     }
 }
 
+
 void json_stop(req_matcher_t * base)
 {
     json_req_matcher_t *m = (json_req_matcher_t *) base;
     m->stop_requested = 1;
 }
 
+//prints the JSON in a user readiable format
 static int pretty_print_json(char * line , char** json_pretty_text)
 {
     json_t *j_object;
@@ -63,16 +64,18 @@ static int pretty_print_json(char * line , char** json_pretty_text)
     if(!json_is_object(j_object))
     {
         fprintf(stderr,"error: commit data -- is not an object [%.50s]\n", line);
-        json_decref(j_object);
         *json_pretty_text = "Corrupted Json";
+        json_decref(j_object);
         return 0;
     }
     json_text = json_dumps(j_object,JSON_INDENT(indentValue)|JSON_PRESERVE_ORDER);
+
     if(json_text)
     {
         *json_pretty_text = json_text;
     }
-    else {
+    else
+    {
         *json_pretty_text = "Corrupted Json";
         json_decref(j_object);
         return 0;
@@ -81,6 +84,26 @@ static int pretty_print_json(char * line , char** json_pretty_text)
     return 1;
 }
 
+void print_json_request(int request_lines, char **request, int * matchedLines)
+{
+    int i, j;
+    char * json_pretty_text;
+    putchar('\n');
+
+    for (i = 0; i < request_lines ; i++) {
+        if (matchedLines[i]>0) {
+            if (pretty_print_json(request[i], &json_pretty_text)>0) {
+                printf("\n%s\n",json_pretty_text);
+            }
+        }
+    }
+
+    for (j = 0; j < strlen(request[request_lines - 1]) && j < 80; j++)
+     putchar('-');
+
+    putchar('\n');
+    fflush(stdout);
+}
 //Parse and get the time
 static int parse_req_json_time(char *line, ssize_t line_size, time_t * time)
 {
@@ -96,100 +119,59 @@ static int parse_req_json_time(char *line, ssize_t line_size, time_t * time)
     json_error_t j_error;
     char * json_pretty_text;
 
-
-//    printf("\n\n##### HI Processing line %d #####\n", lineCount++);
-
     j_object = json_loads(line, 0, &j_error);
-    if(!json_is_object(j_object))
-    {
-        fprintf(stderr,"error: commit data -- is not an object [%.50s]\n", line);
+    if(!json_is_object(j_object)){
+        fprintf(stderr,"error: commit data -- is not a valid JSON object [%.50s]\n", line);
         json_decref(j_object);
         return -1;
     }
-
-//    pretty_print_json(line, &json_pretty_text);
-//    printf("\n%s",json_pretty_text);
 
     j_time = json_object_get(j_object, "time");
     message_text = json_string_value(j_time);
     strptime(message_text, "%Y-%m-%d %H:%M:%S", &request_tm);
     *time = mktime(&request_tm);
 
-    json_decref(j_object);
+    json_decref(j_object); //dereference the mem for JANSSON
 
     return (1);
 }
 
-//Change this function to parse as words rather then exp
 static int json_process_line(req_matcher_t * base, char *line, ssize_t line_size, off_t offset)
 {
     json_req_matcher_t *m = (json_req_matcher_t *) base;
 
-    if ((m->stop_requested) || (line_size == -1)) {
+    // stop the processing if file is empty or end signal is received.
+    if ((m->stop_requested) || (line_size == -1)){
         json_on_request(m, &request);
         return ((m->stop_requested) ? STOP_SIGNAL : EOF_REACHED);
     }
 
     add_to_request(&request, line, offset);
 
-    parse_req_json_time(line, line_size, &(request.time)); //just for testing remove it later
-
     if (request.time == 0) {
         parse_req_json_time(line, line_size, &(request.time));
     }
-
     return (0);
 }
 
-int check_json_request(int lines, char **request, time_t request_time, pcre ** regexps, int num_regexps)
+//check and returns matched
+int check_json_request(int lines, char **request, time_t request_time, pcre ** regexps, int num_regexps, int* matchedLines)
 {
-    int *matches, i, j, matched;
-
-    matches = malloc(sizeof(int) * num_regexps);
-    memset(matches, 0, (sizeof(int) * num_regexps));
+    int i, j, matched;
 
     for (i = 0; i < lines; i++) {
         for (j = 0; j < num_regexps; j++) {
             int ovector[30];
-            if (matches[j])
+            if (matchedLines[i] > 0)
                 continue;
 
-            matched = pcre_exec(regexps[j], NULL, request[i], strlen(request[i]), 0, 0, ovector, 30);
-            if (matched > 0)
-                matches[j] = 1;
+            matchedLines[i] = pcre_exec(regexps[j], NULL, request[i], strlen(request[i]), 0, 0, ovector, 30);
+            if (matchedLines[i] > 0){
+                matched = 1;
+            }
         }
     }
-
-    matched = 1;
-    for (j = 0; j < num_regexps; j++) {
-        matched &= matches[j];
-    }
-
-    free(matches);
-    return (matched);
-}
-
-
-void print_json_request(int request_lines, char **request)
-{
-    int i, j;
-    char * json_pretty_text;
-
-    putchar('\n');
-
-    for (i = 0; i < request_lines; i++)
-    {
-        if (pretty_print_json(request[i], &json_pretty_text)>0)
-        {
-            printf("\n%s\n",json_pretty_text);
-        }
-    }
-
-    for (j = 0; j < strlen(request[request_lines - 1]) && j < 80; j++)
-        putchar('-');
-
-    putchar('\n');
-    fflush(stdout);
+    return matched;
 }
 
 void handle_json_request(request_t * req, void *cxt_arg)
@@ -198,16 +180,16 @@ void handle_json_request(request_t * req, void *cxt_arg)
     context_t *cxt = (context_t *) cxt_arg;
     req_matcher_t * req_matcher = (req_matcher_t *)req;
 
+    int* matchedLines = malloc(sizeof(int) * req->lines);
+    memset(matchedLines, 0, (sizeof(int) * req->lines)); //initialize
+
     if ((req->time > cxt->start_time &&
-    check_json_request(req->lines, req->buf, req->time, cxt->regexps, cxt->num_regexps))) {
+         check_json_request(req->lines, req->buf, req->time, cxt->regexps, cxt->num_regexps, matchedLines))) {
         if (req->time != 0) {
             printf("@@%lu\n", req->time);
         }
 
-        printf("\n#################### Here\n");
-
-        print_json_request(req->lines, req->buf);
-
+        print_json_request(req->lines, req->buf, matchedLines);
     }
     if (req->time > time) {
         time = req->time;
@@ -217,7 +199,6 @@ void handle_json_request(request_t * req, void *cxt_arg)
         cxt->m->stop(cxt->m);
     }
 }
-
 
 //
 req_matcher_t *json_req_matcher(on_req fn1, on_err fn2, void *arg)
